911171623cae6c7878f35aa5d3006d05c1d58af8
Lucas Conde <lucasconde742@gmail.com>
2025-08-24 15:26:00 -0300

    Actulizando contraseña segura

--- php/auth/check.php
+++ php/auth/check.php
@@ -1,42 +1,41 @@
 <?php
-
     session_start();
 
     include("../../config/conexion.php");
 
-    if(isset($_POST['user']) && isset($_POST['password'])){
-        $resultado = $conexion->query("SELECT *
-                                            FROM usuarios
-                                            WHERE user ='".$_POST['user']."' AND
-                                            password='".$_POST['password']."' LIMIT 1")or die($conexion->error);
+    if (isset($_POST['user']) && isset($_POST['password'])) {
+        $user = $_POST['user'];
+        $password = $_POST['password'];
 
-        if(mysqli_num_rows($resultado)> 0){
+        // Aplicar SHA2 directamente en la consulta
+        $resultado = $conexion->query("SELECT * 
+                                    FROM usuarios 
+                                    WHERE user = '$user' 
+                                    AND password = SHA2('$password', 256) 
+                                    LIMIT 1") or die($conexion->error);
 
+        if (mysqli_num_rows($resultado) > 0) {
             $datos_usuario = mysqli_fetch_row($resultado);
 
-
-            $_SESSION['usuario'] = $usuario;
-            $_SESSION['ultimoAcceso'] = date("Y-n-j H:i:s");
-
-            // ID
+            // Extraer datos
             $id = $datos_usuario[0];
-            // USUARIO
             $usuario = $datos_usuario[1];
-            // PASSWORD
-            $password = $datos_usuario[2];
+            $password_hash = $datos_usuario[2];
 
+            // Guardar en sesión
             $_SESSION['usuario'] = array(
                 'id' => $id,
                 'usuario' => $usuario,
-                'password' => $password
+                'password' => $password_hash
             );
+            $_SESSION['ultimoAcceso'] = date("Y-n-j H:i:s");
+
             header("Location: http://lucasconde.ddns.net/Balance/php/pages/dashboard.php");
-        }else{
-            ?>
-                <script>
-                    alert("Usuario o password incorrectos.");
-                    window.location.href = "http://lucasconde.ddns.net/Balance";
-                </script>
-            <?php
+        } else {
+            echo "<script>
+                    alert('Usuario o password incorrectos.');
+                    window.location.href = 'http://lucasconde.ddns.net/Balance';
+                </script>";
         }
     }
+?>
